e3253b5830fcfa3e71d03f80593b025b188bab98
ruofeng <huangruofeng33@gmail.com>
2025-03-09 17:54:32 +0800

    。

--- app/(content)/creation/motion/page.tsx
+++ app/(content)/creation/motion/page.tsx
@@ -1,10 +1,11 @@
 import VideoGallery from '@/app/components/video-gallary';
 import creation from '@/app/config/media/creation.json';
+import { VideoItem } from '@/app/utils/type';
 
 export default function MotionPage() {
   return (
     <div className="space-y-6">
-      <VideoGallery mediaJSON={creation.motion} />
+      <VideoGallery mediaJSON={creation.motion as VideoItem[]} />
     </div>
   );
 }
--- app/(content)/photography/video/page.tsx
+++ app/(content)/photography/video/page.tsx
@@ -1,10 +1,11 @@
 import VideoGallery from '@/app/components/video-gallary';
 import photography from '@/app/config/media/photography.json';
+import { VideoItem } from '@/app/utils/type';
 
 export default function VideoPage() {
   return (
     <div className="space-y-6">
-      <VideoGallery mediaJSON={photography.video} />
+      <VideoGallery mediaJSON={photography.video as VideoItem[]} />
     </div>
   );
 }
